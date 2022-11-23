9258957970691be69ce61a1821c60aef602cc69e
leezhiwei <zhiweilee.com@gmail.com>
2022-11-23 13:49:47 +0800

    Use JS to randomise Linux command.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-const hello = "Hello";
-console.log(hello);
+const textarray = ["cd Desktop","ls -la","uname -a","ssh root@localhost","sudo apt update","dmesg | tail"];
+const index = Math.floor(Math.random() * textarray.length);
+const text = textarray[index];
+for (let x = 0; x < text.length; x++){
+    document.querySelector('.lintext').textContent += text[x];
+}
