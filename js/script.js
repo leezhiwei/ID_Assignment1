e26e39bc20ee7de23a5767552a0a08b26c6d77ea
leezhiwei <zhiweilee.com@gmail.com>
2022-11-23 14:02:00 +0800

    Make JS look cleaner, assigned queryselect to var.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const textarray = ["cd Desktop","ls -la","uname -a","ssh root@localhost","sudo apt update","dmesg | tail"];
 const index = Math.floor(Math.random() * textarray.length);
 const text = textarray[index];
+const changedoc = document.querySelector('.lintext')
 for (let x = 0; x < text.length; x++){
-    document.querySelector('.lintext').textContent += text[x];
+    changedoc.textContent += text[x];
 }
